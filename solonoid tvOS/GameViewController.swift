f34a4e57537edfbced073c3ca0e9ec3d1f6b3042
Oddur Sigurdsson <oddurs@gmail.com>
2026-01-06 13:28:21 -0500

    Initial Commit

---
+++ solonoid tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  solonoid tvOS
+//
+//  Created by Oddur Sigurdsson on 1/6/26.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
